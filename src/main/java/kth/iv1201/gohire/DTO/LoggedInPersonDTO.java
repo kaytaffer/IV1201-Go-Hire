bb515c477c82ef32c214a738bc43f38ed483b81c
wPatr <91604767+wPatr@users.noreply.github.com>
2024-02-06 12:25:35 +0100

    Created DTO containing information about a logged in persons <code>username</code> and <code>role</code>.
    Co-Authored-By: Kevin O'Flaherty <90850365+kaytaffer@users.noreply.github.com>

---
+++ src/main/java/kth/iv1201/gohire/DTO/LoggedInPersonDTO.java
@@ -0,0 +1,34 @@
+package kth.iv1201.gohire.DTO;
+
+/**
+ * DTO containing information about a logged in persons <code>username</code> and <code>role</code>.
+ */
+public class LoggedInPersonDTO {
+    Integer id;
+    String username;
+    String role;
+
+    /**
+     * Creates an instance of a <code>LoggedInPersonDTO</code>.
+     * @param id The id associated with the logged in person.
+     * @param username The person's username.
+     * @param role The person's role as a user of the application.
+     */
+    public LoggedInPersonDTO(Integer id, String username, String role) {
+        this.id = id;
+        this.username = username;
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
