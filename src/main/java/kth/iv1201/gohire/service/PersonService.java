bf2aecfaa6e9472786c279f52cc2bbf8ca51f72a
wPatr <91604767+wPatr@users.noreply.github.com>
2024-02-06 12:42:38 +0100

    Implemented login method for user authentication.
    Co-Authored-By: Kevin O'Flaherty <90850365+kaytaffer@users.noreply.github.com>

--- src/main/java/kth/iv1201/gohire/service/PersonService.java
+++ src/main/java/kth/iv1201/gohire/service/PersonService.java
@@ -1,4 +1,39 @@
 package kth.iv1201.gohire.service;
 
+import kth.iv1201.gohire.DTO.LoggedInPersonDTO;
+import kth.iv1201.gohire.entity.PersonEntity;
+import kth.iv1201.gohire.exception.LoginFailedException;
+import kth.iv1201.gohire.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service Class that handles business logic related to persons.
+ */
+@Service
 public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    /**
+     * Fetches a <code>LoggedInPersonDTO</code> matching the credentials entered by the user.
+     * @param username The username entered by the user.
+     * @param password The password entered by the user.
+     * @return <code>LoggedInPersonDTO</code> representing the logged-in user matching the entered credentials.
+     * @throws LoginFailedException when a matching user does not exist in the database.
+     */
+    public LoggedInPersonDTO login(String username, String password) throws LoginFailedException {
+        PersonEntity personEntity = personRepository.findByUsernameAndPassword(username, password);
+        if(personEntity == null){
+            throw new LoginFailedException("Person with given credentials does not exist");
+        }
+        return new LoggedInPersonDTO(personEntity.getId(), personEntity.getUsername(), personEntity.getRole().getName());
+    }
+
+
 }
