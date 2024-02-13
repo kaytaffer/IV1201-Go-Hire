ca1e238d37c64d430eb7f98f0ae7bda5daa4bc93
vilmelhina <vilhelmina2.0@gmail.com>
2024-02-13 10:07:50 +0100

    Added transaction handling to PersonService, PersonRepository and RoleRepository
    Co-authored-by: Noxiium <johan98nox@gmail.com>

--- src/main/java/kth/iv1201/gohire/repository/PersonRepository.java
+++ src/main/java/kth/iv1201/gohire/repository/PersonRepository.java
@@ -3,11 +3,14 @@ package kth.iv1201.gohire.repository;
 import kth.iv1201.gohire.entity.PersonEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Repository responsible for accessing data related to <code>PersonEntity</code>s.
  */
 @Repository
+@Transactional(propagation = Propagation.MANDATORY)
 public interface PersonRepository extends JpaRepository<PersonEntity, Integer> {
     PersonEntity findByUsernameAndPassword(String username, String password);
 }
