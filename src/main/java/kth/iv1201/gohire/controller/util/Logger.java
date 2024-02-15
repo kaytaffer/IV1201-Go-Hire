5e5af16589cc392e586ad313f74acc956e1ff753
kaytaffer <kevinheltenkelt@gmail.com>
2024-02-15 10:36:29 +0100

    Preliminary structure of Logger implemented
    Co-authored-by: Patricia Lagerhult <91604767+wpatr@users.noreply.github.com>

---
+++ src/main/java/kth/iv1201/gohire/controller/util/Logger.java
@@ -0,0 +1,58 @@
+package kth.iv1201.gohire.controller.util;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+/**
+ * Class concerning static methods to write to error and event logs.
+ */
+public class Logger {
+
+    private final static String ERROR_LOG_NAME = "errorlog.txt";
+    private final static String EVENT_LOG_NAME = "eventlog.txt";
+
+    /**
+     * Logs the supplied exception to the errorlog, prepending the current time.
+     * @param exceptionToLog The error to write to the log.
+     */
+    public static void logError(Exception exceptionToLog) {
+        FileWriter exceptionLogger = makeWriter(ERROR_LOG_NAME);
+        String message = "";
+        //TODO decide how to log, stackTrace?
+        makeLogEntry(exceptionLogger, message);
+    }
+
+    /**
+     * Logs an event with the supplied message, prepending the current time of the event.
+     * @param message The message to write to the log.
+     */
+    public static void logEvent(String message) {
+        FileWriter eventLogger = makeWriter(EVENT_LOG_NAME);
+        makeLogEntry(eventLogger, message);
+    }
+
+    //Creates a named FileWriter
+    private static FileWriter makeWriter(String logName) {
+        try (FileWriter fileWriter = new FileWriter(logName, true)) {
+            return fileWriter;
+        } catch (IOException e) {
+            //TODO don't infiniloop
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Writes the supplied message to the supplied FileWriter
+    private static void makeLogEntry(FileWriter fileWriter, String message) {
+        String time = LocalDateTime.now().toString();
+        message = time + ": " + message;
+        try {
+            fileWriter.append(message);
+            fileWriter.close();
+        } catch (IOException e) {
+            //TODO don't infiniloop
+            throw new RuntimeException(e);
+        }
+    }
+
+}
