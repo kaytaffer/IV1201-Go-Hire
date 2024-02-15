74047815ad017bb1f23f1252c701927e613c7706
kaytaffer <kevinheltenkelt@gmail.com>
2024-02-15 14:42:51 +0100

    Errorlogging implemented
    Co-authored-by: Patricia Lagerhult <91604767+wpatr@users.noreply.github.com>

--- src/main/java/kth/iv1201/gohire/controller/PersonController.java
+++ src/main/java/kth/iv1201/gohire/controller/PersonController.java
@@ -5,6 +5,7 @@ import kth.iv1201.gohire.DTO.CreateApplicantRequestDTO;
 import kth.iv1201.gohire.DTO.LoggedInPersonDTO;
 import kth.iv1201.gohire.DTO.LoginRequestDTO;
 import kth.iv1201.gohire.controller.util.Logger;
+import kth.iv1201.gohire.controller.util.LoggerException;
 import kth.iv1201.gohire.service.PersonService;
 import kth.iv1201.gohire.service.exception.LoginFailedException;
 import kth.iv1201.gohire.service.exception.UserCreationFailedException;
@@ -33,11 +34,12 @@ public class PersonController {
      * Handles the login API-request
      * @param loginRequest DTO containing login request data
      * @throws LoginFailedException If the username and password do not match an existing user.
+     * @throws LoggerException if there is a problem with logging an event.
      * @return <code>LoggedInPersonDTO</code> representing the logged-in user
      */
     @PostMapping("/login")
     public LoggedInPersonDTO login(@RequestBody @Valid LoginRequestDTO loginRequest)
-            throws LoginFailedException {
+            throws LoginFailedException, LoggerException {
         LoggedInPersonDTO newApplicant = personService.login(loginRequest);
         Logger.logEvent("User logged in: " + newApplicant.getUsername());
         return newApplicant;
@@ -48,10 +50,11 @@ public class PersonController {
      * @param createApplicantRequest DTO containing applicant request data.
      * @return <code>LoggedInPersonDTO</code> representing the newly created and logged-in user
      * @throws UserCreationFailedException If the requested username already exists.
+     * @throws LoggerException if there is a problem with logging an event.
      */
     @PostMapping("/createApplicant")
     public LoggedInPersonDTO createNewApplicant(@RequestBody @Valid CreateApplicantRequestDTO createApplicantRequest)
-            throws UserCreationFailedException {
+            throws UserCreationFailedException, LoggerException {
         LoggedInPersonDTO newApplicant = personService.createApplicantAccount(createApplicantRequest);
         Logger.logEvent("New applicant registered: " + newApplicant.getUsername());
         return newApplicant;
--- src/main/java/kth/iv1201/gohire/controller/util/Logger.java
+++ src/main/java/kth/iv1201/gohire/controller/util/Logger.java
@@ -2,8 +2,10 @@ package kth.iv1201.gohire.controller.util;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 /**
  * Utility class containing static methods to write to error and event logs.
@@ -16,44 +18,43 @@ public class Logger {
     /**
      * Logs the supplied exception to the errorlog, prepending the current time.
      * @param exceptionToLog The error to write to the log.
+     * @throws LoggerException if there is a problem with logging an event.
      */
-    public static void logError(Exception exceptionToLog) {
+    public static void logError(Exception exceptionToLog) throws LoggerException {
         FileWriter exceptionLogger = makeWriter(ERROR_LOG_NAME);
-        String message = "";
-        //TODO decide how to log, stackTrace?
+        String message = Arrays.toString(exceptionToLog.getStackTrace());
         makeLogEntry(exceptionLogger, message);
     }
 
     /**
      * Logs an event with the supplied message, prepending the current time of the event.
      * @param message The message to write to the log.
+     * @throws LoggerException if there is a problem with logging an event.
      */
-    public static void logEvent(String message) {
+    public static void logEvent(String message) throws LoggerException {
         FileWriter eventLogger = makeWriter(EVENT_LOG_NAME);
         makeLogEntry(eventLogger, message);
     }
 
     //Creates a named FileWriter
-    private static FileWriter makeWriter(String logName) {
+    private static FileWriter makeWriter(String logName) throws LoggerException {
         try {
             String date = LocalDate.now().toString();
             return new FileWriter(date + "_" + logName, true);
         } catch (IOException e) {
-            //TODO don't infiniloop
-            throw new RuntimeException(e);
+            throw new LoggerException("Logger failed to write to log. Caused by: \n" + Arrays.toString(e.getStackTrace()));
         }
     }
 
     //Writes the supplied message to the supplied FileWriter and closes it
-    private static void makeLogEntry(FileWriter fileWriter, String message) {
+    private static void makeLogEntry(FileWriter fileWriter, String message) throws LoggerException {
         String time = LocalDateTime.now().toString();
         message = time + ": " + message + "\n";
         try {
             fileWriter.append(message);
             fileWriter.close();
         } catch (IOException e) {
-            //TODO don't infiniloop
-            throw new RuntimeException(e);
+            throw new LoggerException("Logger failed connect to log file. Caused by: \n" + Arrays.toString(e.getStackTrace()));
         }
     }
 
