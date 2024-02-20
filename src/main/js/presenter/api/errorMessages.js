a5b7d32a2571999507cb4a927fe7fef3c92599a9
kaytaffer <kevinheltenkelt@gmail.com>
2024-02-20 15:50:43 +0100

    Frontend implemented server caused error handling. Also, login related errors shown to user
    Co-authored-by: Patricia Lagerhult <91604767+wpatr@users.noreply.github.com>

---
+++ src/main/js/presenter/api/errorMessages.js
@@ -0,0 +1,3 @@
+export const LOGIN_FAIL = {errorType: "LOGIN_FAIL", message: "Username and password do not match."}
+export const USER_INPUT_ERROR = {errorType: "USER_INPUT_ERROR", message: "Please follow the form's required input examples."}
+export const USERNAME_ALREADY_EXISTS = {errorType: "USERNAME_ALREADY_EXISTS", message: "The suggested username already exists."}
--- src/main/js/presenter/login.js
+++ src/main/js/presenter/login.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import {LoginView} from "../view/loginView";
 import {authenticateLogin, createNewApplicant} from "./api/apiCallHandler";
 import {CreateNewApplicantView} from "../view/createNewApplicantView";
+import {LOGIN_FAIL, USER_INPUT_ERROR, USERNAME_ALREADY_EXISTS} from "./api/errorMessages";
 
 /**
  * Responsible for the logic of the login page
@@ -13,26 +14,31 @@ import {CreateNewApplicantView} from "../view/createNewApplicantView";
 export function Login(props){
 
     const [newUserIsCreated, setNewUserIsCreated] = useState(false)
+    const [errorMessage, setErrorMessage] = useState(null)
 
     function catchPromiseError(error) {
-        //TODO Handle promise error
+        if (error.message === LOGIN_FAIL.errorType)
+            setErrorMessage(LOGIN_FAIL.message)
+        else if (error.message === USER_INPUT_ERROR.errorType)
+            setErrorMessage(USER_INPUT_ERROR.message)
+        else if (error.message === USERNAME_ALREADY_EXISTS.errorType) {
+            setErrorMessage(USERNAME_ALREADY_EXISTS.message) }
     }
 
     function login(username, password) {
         authenticateLogin(username, password).then(user => props.onLoggedIn(user))
             .catch(catchPromiseError)
-
     }
 
     function newApplicant(firstName, lastName, email, personNumber, username, password) {
         createNewApplicant(firstName, lastName, email, personNumber, username, password)
             .then(user => {if(user) setNewUserIsCreated(true)}).catch(catchPromiseError)
-
     }
 
     return (<div>
         <LoginView onLogin={login}/>
         {newUserIsCreated ? <div>Account successfully created</div> : <CreateNewApplicantView onCreate={newApplicant}/>}
+            {errorMessage && <div>{errorMessage}</div>}
     </div>
     )
 }
