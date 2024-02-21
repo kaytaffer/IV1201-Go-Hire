15d1f3c2e16af0ffb951a2bf0eb444175c0a66cb
vilmelhina <vilhelmina2.0@gmail.com>
2024-02-21 09:33:11 +0100

    Moved error message rendering from presenter to view
    Co-authored-by: Noxiium <johan98nox@gmail.com>

--- src/main/js/presenter/login.js
+++ src/main/js/presenter/login.js
@@ -3,6 +3,7 @@ import {LoginView} from "../view/loginView";
 import {authenticateLogin, createNewApplicant} from "./api/apiCallHandler";
 import {CreateNewApplicantView} from "../view/createNewApplicantView";
 import {LOGIN_FAIL, USER_INPUT_ERROR, USERNAME_ALREADY_EXISTS} from "./api/errorMessages";
+import {UserNoticeView} from "../view/userNoticeView";
 
 /**
  * Responsible for the logic of the login page
@@ -35,10 +36,12 @@ export function Login(props){
             .then(user => {if(user) setNewUserIsCreated(true)}).catch(catchPromiseError)
     }
 
-    return (<div>
-        <LoginView onLogin={login}/>
-        {newUserIsCreated ? <div>Account successfully created</div> : <CreateNewApplicantView onCreate={newApplicant}/>}
-            {errorMessage && <div>{errorMessage}</div>}
-    </div>
+    return (
+        <div>
+            <LoginView onLogin={login}/>
+            {newUserIsCreated ? <UserNoticeView message={"Account successfully created"} error={false}/> :
+                            <CreateNewApplicantView onCreate={newApplicant}/>}
+            {errorMessage && <UserNoticeView message={errorMessage} error={true}/>}
+        </div>
     )
 }
---
+++ src/main/js/view/userNoticeView.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+/**
+ * Responsible for rendering user notice messages
+ * @param props props
+ * @param {String} props.message - the message to display to the user
+ * @param {boolean} props.error - true if the message is an error, false if it is information
+ * @returns {JSX.Element} the rendered user notice message
+ */
+export function UserNoticeView(props) {
+    return <div>{props.message}</div>
+}
