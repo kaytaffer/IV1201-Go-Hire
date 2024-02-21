1a6b65aecb9f603c6e97e983ee6b000e5f71eca1
kaytaffer <kevinheltenkelt@gmail.com>
2024-02-21 15:10:22 +0100

    Logic to fetch applications from the server and pass them to relevant views
    Co-authored-by: Patricia Lagerhult <91604767+wpatr@users.noreply.github.com>

--- src/main/js/view/homePageRecruiterView.js
+++ src/main/js/view/homePageRecruiterView.js
@@ -1,17 +1,21 @@
 import React from "react";
+import {ApplicationListView} from "./applicationListView";
 
 /**
  * Responsible for rendering the home page for recruiter
  * @param props - props
  * @param {Object} props.user - the current logged-in user
+ * @param {Array} props.applications - list of application objects
+ * @param {function} props.onShowApplications - called when user clicks the showApplications button
  * @returns {JSX.Element} the rendered recruiter home page
- * @constructor
  */
 export function HomePageRecruiterView(props) {
     return (
         <div>
             <h1>Recruiter</h1>
             <p>Welcome {props.user.username}</p>
+            {props.applications && <ApplicationListView props={props.applications}/>}
+            <p><button id={"showApplications"} onClick={props.onShowApplications}>Display applications</button></p>
         </div>
     )
 }
