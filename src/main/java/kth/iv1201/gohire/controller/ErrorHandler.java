639fd606a392bca94a153ffb223296ac4b45611f
wPatr <91604767+wPatr@users.noreply.github.com>
2024-02-21 12:06:16 +0100

    Method now throws correct exception
    Co-Authored-By: Kevin O'Flaherty <90850365+kaytaffer@users.noreply.github.com>

--- src/main/java/kth/iv1201/gohire/controller/ErrorHandler.java
+++ src/main/java/kth/iv1201/gohire/controller/ErrorHandler.java
@@ -3,11 +3,11 @@ package kth.iv1201.gohire.controller;
 import kth.iv1201.gohire.DTO.ErrorDTO;
 import kth.iv1201.gohire.controller.util.Logger;
 import kth.iv1201.gohire.controller.util.LoggerException;
-import kth.iv1201.gohire.service.exception.LoginFailedException;
 import kth.iv1201.gohire.service.exception.UserCreationFailedException;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -27,15 +27,16 @@ public class ErrorHandler implements ErrorController {
      * @return ErrorDTO containing <code>ErrorType</code> and appropriate message to client, or null if causing
      *         <code>Exception</code> isn't meant to be translated to client.
      */
-    @ExceptionHandler({UserCreationFailedException.class, LoggerException.class,
-            MethodArgumentNotValidException.class, LoginFailedException.class})
+    @ExceptionHandler({Exception.class})
     public ResponseEntity<ErrorDTO> handleException (Exception exception) throws LoggerException {
+        System.out.println(exception.toString());
+
         if (exception instanceof UserCreationFailedException) {
             return handleUserCreationFailedException((UserCreationFailedException) exception);
         } else if (exception instanceof MethodArgumentNotValidException) {
             return handleMethodArgumentNotValidException((MethodArgumentNotValidException) exception);
-        } else if (exception instanceof LoginFailedException) {
-            return handleLoginFailedException((LoginFailedException) exception);
+        } else if (exception instanceof BadCredentialsException) {
+            return handleBadCredentialsException((BadCredentialsException) exception);
         } else if (exception instanceof LoggerException) {
             return handleLoggerException((LoggerException) exception);
         } else
@@ -51,7 +52,7 @@ public class ErrorHandler implements ErrorController {
         return new ResponseEntity<> (new ErrorDTO(ErrorType.USER_INPUT_ERROR, exception.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
-    private ResponseEntity<ErrorDTO> handleLoginFailedException(LoginFailedException exception){
+    private ResponseEntity<ErrorDTO> handleBadCredentialsException(BadCredentialsException exception){
         return new ResponseEntity<> (new ErrorDTO(ErrorType.LOGIN_FAIL,exception.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 
@@ -63,7 +64,7 @@ public class ErrorHandler implements ErrorController {
     //Generic handling and logging for all unspecified exceptions
     private ResponseEntity<ErrorDTO> handleOtherExceptions(Exception exception) throws LoggerException {
         Logger.logError(exception);
-        return null;
+        return new ResponseEntity<> (new ErrorDTO(ErrorType.SERVER_INTERNAL,exception.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
