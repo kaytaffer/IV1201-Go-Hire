7fccddc631a2e9ddaaadaae148c35430e3088403
Noxiium <johan98nox@gmail.com>
2024-02-29 11:21:06 +0100

    Added javadoc

--- src/main/js/presenter/api/apiCallHandler.js
+++ src/main/js/presenter/api/apiCallHandler.js
@@ -59,11 +59,16 @@ export function createNewApplicant(firstName, lastName, email, personNumber, use
 
 /**
  * Tasks server to fetch all applications
- * @returns {Promise<any>} a promise either resolving to a object containing a list of applicants  or an error object
+ * @returns {Promise<any>} a promise either resolving to a object containing a list of applicants or an error object
  */
 export function fetchListOfApplications() {
     return sendGetRequest('/applications')
 }
+
+/**
+ * Tasks server to logout user
+ * @returns {Promise<any>} a promise containing an empty body or an error response
+ */
 
 export function logout(){
     //return sendPostRequest('/logout', {});
