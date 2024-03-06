f6c84391202c47251e90bf1a5dbff76f3e90d600
vilmelhina <vilhelmina2.0@gmail.com>
2024-03-06 12:03:05 +0100

    Translated error messaged

--- src/main/js/view/i18n/translations.js
+++ src/main/js/view/i18n/translations.js
@@ -7,14 +7,32 @@ export const translations = {
         "name": "English",
         "translation": {
             "logout": "Logout",
-            "logged-in-as": "Logged in as"
+            "logged-in-as": "Logged in as",
+            "handle": "handle",
+            "account-successfully-created": "Account successfully created",
+            "application-already-handled": "This application has already been handled.",
+            "no-access-rights": "You do not have access rights to this.",
+            "credentials-do-not-match": "Username and password do not match.",
+            "something-went-wrong": "Something went wrong, please try again later.",
+            "wrong-input": "Please follow the form's required input examples.",
+            "username-exists": "The suggested username already exists.",
+            "page-does-not-exist": "The requested page or resource does not exist or is unavailable."
         }
     },
     "sv": {
         "name": "Svenska",
         "translation": {
             "logout": "Logga ut",
-            "logged-in-as": "Inloggad som"
+            "logged-in-as": "Inloggad som",
+            "handle": "hantera",
+            "account-successfully-created": "Konto skapat",
+            "application-already-handled": "Den här ansökan har redan behandlats.",
+            "no-access-rights": "Du har inte behörighet att göra detta.",
+            "credentials-do-not-match": "Användarnamn och lösenord matchar inte.",
+            "something-went-wrong": "Något gick fel, försök igen senare.",
+            "wrong-input": "Följ formulärets input-exempel.",
+            "username-exists": "Användarnamnet existerar redan.",
+            "page-does-not-exist": "Den efterfrågade sidan eller resursen finns inte, eller är otillgänglig."
         }
     }
 }
