8de95ae564cc4033fed7ec004c4b4c9d26fdf5fd
kaytaffer <kevinheltenkelt@gmail.com>
2024-03-07 08:30:22 +0100

    More applicants to show.
    Co-authored-by: Patricia Lagerhult <91604767+wpatr@users.noreply.github.com>

--- src/test/resources/acceptance-test-data.sql
+++ src/test/resources/acceptance-test-data.sql
@@ -12,7 +12,11 @@ INSERT INTO application_status (application_status_id, status) VALUES (2, 'rejec
 INSERT INTO application_status (application_status_id, status) VALUES (3, 'unhandled');
 
 INSERT INTO person (name, surname, pnr, email, password, username, role_id, application_status_id) VALUES
-    ('validApplicantUser', 'validApplicantUser', '19909090-9090', 'email@kth.se', '$2a$10$ZVeEAO5d9j5SyUmUzIfiiuHE/UpKkeFrY5qut6lKUH3cGPBP1u2iq', 'validApplicantUser', 2, 3);
+    ('applicant1', 'validApplicantUser', '19909090-9090', 'email@kth.se', '$2a$10$ZVeEAO5d9j5SyUmUzIfiiuHE/UpKkeFrY5qut6lKUH3cGPBP1u2iq', 'validApplicantUser', 2, 3);
+INSERT INTO person (name, surname, pnr, email, password, username, role_id, application_status_id) VALUES
+    ('applicant2', 'validApplicantUser', '19909090-9090', 'email@kth.se', 'Icantlogin', 'applicant2', 2, 2);
+INSERT INTO person (name, surname, pnr, email, password, username, role_id, application_status_id) VALUES
+    ('applicant3', 'validApplicantUser', '19909090-9090', 'email@kth.se', 'Ialsocantlogin', 'applicant3', 2, 1);
 
 INSERT INTO person (name, surname, pnr, email, password, username, role_id, application_status_id) VALUES
     ('validRecruiterUser', 'validRecruiterUser', '19909090-9090', 'email@kth.se', '$2a$10$9u.byUPFFXkM.GRdzZ5.1eIXuag5fXDkPq3jjSvUh3Azrqt23k9/G', 'validRecruiterUser', 1, null);
